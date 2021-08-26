30d902ae0e57976d6a144504c397a0afb42c80c5
adji142 <prasetyoajiw@gmail.com>
2021-08-26 20:17:03 +0700

    initial debug 3.7

--- application/controllers/API/API_Test.php
+++ application/controllers/API/API_Test.php
@@ -24,42 +24,53 @@ class API_Test extends CI_Controller {
 
 		// echo "hayy saya dapat : ".$order_id;
 		$order_id = $notif->order_id;
-		$registrationIds = array($order_id);
+		// $order_id = '2021081204799914';
+		$SQL = "
+			SELECT b.token FROM topuppayment a 
+			INNER JOIN users b on a.UserID = b.username
+			WHERE a.NoTransaksi = '".$order_id."'
+		";
+		
+		$rs = $this->db->query($SQL);
+		if ($rs) {
+			// var_dump($rs->row());
+			$registrationIds = array($rs->row()->token);
 
-		// prep the bundle
-		$msg = array
-		(
-		    'message'   => 'here is a message. message',
-		    'title'     => 'This is a title. title',
-		    'subtitle'  => 'This is a subtitle. subtitle',
-		    'tickerText'    => 'Ticker text here...Ticker text here...Ticker text here',
-		    'vibrate'   => 1,
-		    'sound'     => 1,
-		    'largeIcon' => 'large_icon',
-		    'smallIcon' => 'small_icon'
-		);
-		$fields = array
-		(
-		    'registration_ids'  => $registrationIds,
-		    'data'          => $msg
-		);
-		  
-		$headers = array
-		(
-		    'Authorization: key=' . API_ACCESS_KEY,
-		    'Content-Type: application/json'
-		);
-		  
-		$ch = curl_init();
-		curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
-		curl_setopt( $ch,CURLOPT_POST, true );
-		curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-		curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-		curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-		curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-		$result = curl_exec($ch );
-		curl_close( $ch );
-		echo $result;
+			// prep the bundle
+			$msg = array
+			(
+			    'message'   => 'here is a message. message',
+			    'title'     => 'This is a title. title',
+			    'subtitle'  => 'This is a subtitle. subtitle',
+			    'tickerText'    => 'Ticker text here...Ticker text here...Ticker text here',
+			    'vibrate'   => 1,
+			    'sound'     => 1,
+			    'largeIcon' => 'large_icon',
+			    'smallIcon' => 'small_icon'
+			);
+			$fields = array
+			(
+			    'registration_ids'  => $registrationIds,
+			    'data'          => $msg
+			);
+			  
+			$headers = array
+			(
+			    'Authorization: key=' . API_ACCESS_KEY,
+			    'Content-Type: application/json'
+			);
+			  
+			$ch = curl_init();
+			curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+			curl_setopt( $ch,CURLOPT_POST, true );
+			curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+			curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+			curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+			$result = curl_exec($ch );
+			curl_close( $ch );
+			echo $result;	
+		}
 	}
 
 }
