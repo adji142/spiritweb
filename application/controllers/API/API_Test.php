751465f8307ce565a273f0279b0330941af01a56
adji142 <prasetyoajiw@gmail.com>
2021-09-03 12:54:50 +0700

    initial debug 6.6

--- application/controllers/API/API_Test.php
+++ application/controllers/API/API_Test.php
@@ -17,7 +17,7 @@ class API_Test extends CI_Controller {
 		// $this->db->query("insert into testCron values(now())");
 		define( 'API_ACCESS_KEY', 'AAAAWRnKigc:APA91bF2DUbxrbIws3clI_lGq40MMbc0x9hjYZjf6xyTGukNVb8BrgIWYTMnz6NB2-ZdGYpVSo2UuKjz3YaVcN777aIU-dGNdTdEYKRtRwYMF0s8gJu5oPLg8zoivTAPQf_pZASw0w4A' );
 
-		$notification = array("token"=>"","notification"=>array());
+		$notification = array("registration_ids"=>"","notification"=>array());
 
 		\Midtrans\Config::$isProduction = false;
 		\Midtrans\Config::$serverKey = $this->ModelsExecuteMaster->midTransServerKey();
@@ -74,7 +74,7 @@ class API_Test extends CI_Controller {
 			curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
 			curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
 			curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-			curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+			curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $notification ) );
 			$result = curl_exec($ch );
 			curl_close( $ch );
 			echo $result;	
